b6e484d813d7aaf39958d00300402798840903f8
Lachlan Campbell <lachlan@hackclub.com>
2019-09-29 23:02:15 -0400

    Remove prettier

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -2,8 +2,6 @@
 import { jsx } from 'theme-ui'
 import React, { Component } from 'react'
 import p5 from '../sketches/p5'
-// import prettier from 'prettier/standalone'
-// import parser from 'prettier/parser-flow'
 import theme from '../gatsby-plugin-theme-ui'
 
 export class SketchPreview extends Component {
@@ -39,17 +37,17 @@ export const SketchSource = props => (
       wordWrap: 'break-word',
       my: 3
     }}
-    children={
-      props.sketch
-        .toString()
-        .replace('function _default(p){', '')
-        .replace(/;\}$/, '')
-        .replace(
-          /=function\(\)\{/g,
-          ` = () => {
+    children={props.sketch
+      .toString()
+      .replace('function _default(p){', '')
+      .replace(/;\}$/, '')
+      .replace(
+        /=function\(\)\{/g,
+        ` = () => {
   `
-        )
-      // { parser: 'flow', plugins: [parser], semi: false, singleQuote: true }
-    }
+      )
+      .replace(/\;/g, `  `)
+      .replace(/\,/g, ', ')
+      .replace(/\=/g, ' = ')}
   />
 )
