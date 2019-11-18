f5244173761d9995f9858e3d603138fa71144e2b
Lachlan Campbell <lachlan.campbell@icloud.com>
2019-11-18 02:52:52 -0500

    Use gender-neutral logo

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ export const Banner = props => (
       src={`https://contrast.now.sh/fff/${theme.colors.violetBright.replace(
         '#',
         ''
-      )}?text=%F0%9F%91%A9%F0%9F%8F%BB%E2%80%8D%F0%9F%8E%93&radius=999&size=512&fontSize=2&baseline=1`}
+      )}?text=🧑🏻%E2%80%8D🎓&radius=999&size=512&fontSize=2&baseline=1`}
       alt="Logo"
     />
     <div>
