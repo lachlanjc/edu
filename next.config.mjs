2a6a6a21e51877b259de66c8566c9c655c0e01cf
Lachlan Campbell <lachlanjc@hey.com>
2022-09-05 20:51:32 -0400

    Rewrite essay page requests

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,37 @@
 import { withContentlayer } from 'next-contentlayer'
-import withMDX from '@next/mdx'
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+const config = withContentlayer({
+  swcMinify: true,
+  compiler: {
+    emotion: true,
   },
-})(
-  withContentlayer({
-    swcMinify: true,
-    compiler: {
-      emotion: true,
-    },
-    pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
+  pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
+  images: {
+    domains: ['cdn.glitch.com', 'www.icloud.com'],
+  },
+  experimental: {
     images: {
-      domains: ['cdn.glitch.com', 'www.icloud.com'],
+      allowFutureImage: true,
     },
-    experimental: {
-      images: {
-        allowFutureImage: true,
+  },
+  async rewrites() {
+    return [
+      { source: '/feed.xml', destination: '/_next/static/feed.xml' },
+      {
+        source: '/one_day_this_kid',
+        destination:
+          'https://ima-lhyksfx2l-lachlanjc.vercel.app/one_day_this_kid/',
       },
-    },
-    async rewrites() {
-      return [{ source: '/feed.xml', destination: '/_next/static/feed.xml' }]
-    },
-  }),
-)
+      {
+        source: '/imagining_worlds_post_human',
+        destination:
+          'https://ima-lhyksfx2l-lachlanjc.vercel.app/imagining_worlds_post_human/',
+      },
+    ]
+  },
+})
 
 export default config
