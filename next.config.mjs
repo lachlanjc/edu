6e6d45cf472cbc2cffca17ea26306e57a48ff71e
Lachlan Campbell <lachlanjc@hey.com>
2022-09-05 20:50:34 -0400

    Add MDX plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,34 @@
 import { withContentlayer } from 'next-contentlayer'
+import withMDX from '@next/mdx'
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withContentlayer({
-  swcMinify: true,
-  compiler: {
-    emotion: true,
+const config = withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
   },
-  pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
-  images: {
-    domains: ['cdn.glitch.com', 'www.icloud.com'],
-  },
-  experimental: {
-    images: {
-      allowFutureImage: true,
+})(
+  withContentlayer({
+    swcMinify: true,
+    compiler: {
+      emotion: true,
     },
-  },
-  async rewrites() {
-    return [{ source: '/feed.xml', destination: '/_next/static/feed.xml' }]
-  },
-})
+    pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
+    images: {
+      domains: ['cdn.glitch.com', 'www.icloud.com'],
+    },
+    experimental: {
+      images: {
+        allowFutureImage: true,
+      },
+    },
+    async rewrites() {
+      return [{ source: '/feed.xml', destination: '/_next/static/feed.xml' }]
+    },
+  }),
+)
 
 export default config
